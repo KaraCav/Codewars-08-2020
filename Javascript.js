7656fff5bc0071f2f799a01e8b3a02e9a57a6ea4
Kara Cavanaugh <karacav@msn.com>
2020-08-09 11:33:53 -0400

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -4,3 +4,16 @@
 function arrowArea(a,b) {
  return (a * (b/2)) / 2;
 }
+
+// 2. Find the number of unique items in an array
+function findNumberOfNonRepeats(arr) {
+  let uniqueArr = [arr[0]]
+  for(let i = 1; i < arr.length; i++) {
+    if(!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+  return uniqueArr.length;
+}
+
+findNumberOfNonRepeats(['e',2,4,'f','f','r','f']);
